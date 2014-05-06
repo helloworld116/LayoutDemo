2b749130a53ee2d812f3390d11a051fb62b01d03
helloworld116 <646767424@qq.com>
2014-05-06 17:44:53 +0800

    android layout demo
    study android layout

---
+++ src/com/wzg/layoutdemo/TableLayoutActivity.java
@@ -0,0 +1,11 @@
+package com.wzg.layoutdemo;
+
+import android.app.Activity;
+
+public class TableLayoutActivity extends Activity {
+
+	protected void onCreate(android.os.Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.wzg_tablelayout);
+	};
+}
